99a0fbd5e73136b762eb1414083c2d1a00e56108
Oksana-Pak <okspak7@gmail.com>
2023-03-22 15:09:23 +0200

    add task

---
+++ middelwares/auth.js
@@ -0,0 +1,28 @@
+const jwt = require('jsonwebtoken');
+const { User } = require('../models');
+const { HttpError } = require('../utils');
+// const { JWT_SECRET } = process.env;
+// console.log('secret auth', JWT_SECRET);
+
+const auth = async (req, res, next) => {
+  const { authorization = '' } = req.headers;
+  const [bearer, token] = authorization.split(' ');
+
+  try {
+    if (bearer !== 'Bearer') throw new HttpError(401, 'Not authorized');
+
+    const { id } = jwt.verify(token, process.env.JWT_SECRET);
+
+    const user = await User.findById(id);
+    if (!user || !user.token) throw new HttpError(401, 'Not authorized');
+    req.user = user;
+    next();
+  } catch (error) {
+    if (error.message === 'Invalid signature') {
+      error.status = 401;
+    }
+    next(error);
+  }
+};
+
+module.exports = auth;
